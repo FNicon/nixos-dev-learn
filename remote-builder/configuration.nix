96bdf385aea65262f01a7bdcfc487afc3048a4d8
fnicon <mybeddrom@gmail.com>
2026-02-28 15:54:47 +0900

    add distributed builds

---
+++ remote-builder/configuration.nix
@@ -0,0 +1,7 @@
+{
+  imports = [
+    ./remote-builder.nix
+  ];
+
+  # ...
+}
---
+++ remote-builder/remote-builder.nix
@@ -0,0 +1,30 @@
+{
+  users.users.remotebuild = {
+    isSystemUser = true;
+    group = "remotebuild";
+    useDefaultShell = true;
+
+    openssh.authorizedKeys.keyFiles = [ ./remotebuild.pub ];
+  };
+
+  users.groups.remotebuild = {};
+
+  nix = {
+    nrBuildUsers = 64;
+    settings = {
+      trusted-users = [ "remotebuild" ];
+
+      min-free = 10 * 1024 * 1024;
+      max-free = 200 * 1024 * 1024;
+
+      max-jobs = "auto";
+      cores = 0;
+    };
+  };
+
+  systemd.services.nix-daemon.serviceConfig = {
+    MemoryAccounting = true;
+    MemoryMax = "90%";
+    OOMScoreAdjust = 500;
+  };
+}
