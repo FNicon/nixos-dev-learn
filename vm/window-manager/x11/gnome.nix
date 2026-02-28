7e43fde4e68a8c8b159858ed56c0e21211a33798
fnicon <mybeddrom@gmail.com>
2026-02-28 13:55:58 +0900

    add gnome desktop env sample

---
+++ vm/window-manager/x11/gnome.nix
@@ -0,0 +1,11 @@
+{ config, pkgs, ... }:
+
+{
+  # Enable the X11 windowing system.
+  services.xserver.enable = true;
+
+  # Enable the GNOME Desktop Environment.
+  services.xserver.displayManager.gdm.enable = true;
+  # GNOME
+  services.xserver.desktopManager.gnome.enable = true;
+}
